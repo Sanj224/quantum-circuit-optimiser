8dbfa4d786dddb9f885e3876ba12856815d51244
Sanj224 <sanjana.darbha@gmail.com>
2025-12-06 11:52:34 +0000

    Updated the metrics functions

--- src/quantum_optimiser/multimetric/metrics.py
+++ src/quantum_optimiser/multimetric/metrics.py
@@ -1,21 +1,25 @@
 # Will be used to calculate the metrics from the ZX diagram 
 
 # Qubit count
-def qubitCount(circuit):
-    return len(circuit.qubits)
+def qubit_count(circuit):
+    return circuit.num_qubits
 
 # Number of gates
-def gateCount(circuit):
-    return len(circuit.gates)
+def gate_count(circuit):
+    return len(circuit.data)
 
 # Number of two qubit gates
-def twoQubitGateCount(circuit):
-    return len([gate for gate in circuit.gates if gate.numQubits == 2])
-
+def two_qubit_gate_gount(circuit):
+    return sum(1 for instruction in circuit.data if instruction.operation.num_qubits == 2)
 # Number of T gates
 
-# Number of clifford gates
+def t_gate_count(circuit):
+    return sum(1 for instruction in circuit.data if instruction.operation.name == 't')
 
+# Number of Clifford gates
+def cliffordGateCount(circuit):
+    clifford_gates = {'h', 'x', 'y', 'z', 's', 'sdg', 'cx', 'cy', 'cz', 'swap', 'id', 'i'}
+    return sum(1 for instruction in circuit.data if instruction.operation.name in clifford_gates)
 # fidelity
 
 # Coupling Map
