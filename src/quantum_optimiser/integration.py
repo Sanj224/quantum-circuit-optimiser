071feb3dbf1b5d3a9dd8f03235b37fe1491ded8c
Sanj224 <sanjana.darbha@gmail.com>
2025-12-09 15:12:01 +0000

    Fixed the integration and the metrics

--- src/quantum_optimiser/integration.py
+++ src/quantum_optimiser/integration.py
@@ -1,13 +1,20 @@
 # Since PyZX cannot convert qiskit circuits directly, this script will convert a qiskit circuit to a PyZX circuit
 
 import pyzx
-from qiskit import QuantumCircuit, qasm3
+from qiskit import QuantumCircuit, qasm3, qasm2
 from pyzx.circuit import Circuit
 
-def qiskitToPyZX(qc):
-  qasmCircuit = qasm3.dumps(qc)
-  return Circuit.from_qasm(qasmCircuit)
+def qiskit_to_pyzx(qc):
+  qasm_circuit = qasm3.dumps(qc)
+  circuit = Circuit.from_qasm(qasm_circuit)
+  graph = circuit.to_graph()
+  return graph
 
 # this function will convert a PyZX circuit back to a qiskit circuit
-# def pyzxToQiskit(circuit): 
+def pyzx_to_qiskit(diagram): 
+  pyzx_circuit = Circuit.from_graph(diagram)
+  qasm_circuit = Circuit.to_qasm(pyzx_circuit)
+  circuit = QuantumCircuit.from_qasm_str(qasm_circuit)
+  return circuit
+
 
--- src/quantum_optimiser/multimetric/metrics.py
+++ src/quantum_optimiser/multimetric/metrics.py
@@ -1,27 +1,79 @@
 # Will be used to calculate the metrics from the ZX diagram 
+import pyzx as zx
+import math
 
 # Qubit count
-def qubit_count(circuit):
+def circuit_qubit_count(circuit):
     return circuit.num_qubits
 
 # Number of gates
-def gate_count(circuit):
+def circuit_gate_count(circuit):
     return len(circuit.data)
 
 # Number of two qubit gates
-def two_qubit_gate_gount(circuit):
+def circuit_two_qubit_gate_count(circuit):
     return sum(1 for instruction in circuit.data if instruction.operation.num_qubits == 2)
 # Number of T gates
 
-def t_gate_count(circuit):
+def circuit_t_gate_count(circuit):
     return sum(1 for instruction in circuit.data if instruction.operation.name == 't')
 
 # Number of Clifford gates
-def cliffordGateCount(circuit):
+def circuit_clifford_gate_count(circuit):
     clifford_gates = {'h', 'x', 'y', 'z', 's', 'sdg', 'cx', 'cy', 'cz', 'swap', 'id', 'i'}
     return sum(1 for instruction in circuit.data if instruction.operation.name in clifford_gates)
 # fidelity
 
 # Coupling Map
 
+
+
+# Qubit count
+def graph_qubit_count(graph):
+    return graph.qubit_count()
+
+# Number of gates (vertices excluding inputs/outputs)
+def graph_gate_count(graph):
+    return sum(1 for v in graph.vertices() if graph.type(v) not in [zx.VertexType.BOUNDARY])
+
+# Number of two qubit gates (edges between non-boundary vertices)
+def graph_two_qubit_gate_count(graph):
+    count = 0
+    for edge in graph.edges():
+        v1, v2 = edge
+        if graph.type(v1) != zx.VertexType.BOUNDARY and graph.type(v2) != zx.VertexType.BOUNDARY:
+            count += 1
+    return count
+
+# Number of T gates (Z or X spiders with phase = pi/4 or -pi/4)
+def graph_t_gate_count(graph):
+    count = 0
+    for v in graph.vertices():
+        if graph.type(v) in [zx.VertexType.Z, zx.VertexType.X]:
+            phase = graph.phase(v)
+            if abs(phase - 0.25) < 1e-10 or abs(phase + 0.25) < 1e-10 or abs(phase - 1.75) < 1e-10:
+                count += 1
+    return count
+
+# Number of Clifford gates
+def graph_clifford_gate_count(graph):
+
+    count = 0
+    
+    # Count Clifford spiders (phases that are multiples of π/2)
+    for v in graph.vertices():
+        if graph.type(v) in [zx.VertexType.Z, zx.VertexType.X]:
+            phase = graph.phase(v)
+            # Check if phase is a multiple of 0.5 (which represents π/2 in PyZX)
+            if abs(phase - round(phase * 2) / 2) < 1e-10:
+                count += 1
+        elif graph.type(v) == zx.VertexType.H_BOX:
+            count += 1
+    
+    # Count Hadamard edges
+    for edge in graph.edges():
+        if graph.edge_type(edge) == zx.EdgeType.HADAMARD:
+            count += 1
+    
+    return count
 
